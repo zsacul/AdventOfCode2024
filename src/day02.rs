9ae69e48a7dd5cc633070548e2063ceda2c92d2a
Łukasz Piwowar <zsacul@gmail.com>
2024-12-02 06:31:24 +0100

    day02 wip

--- src/day02.rs
+++ src/day02.rs
@@ -1,46 +1,56 @@
-use std::collections::HashMap;
-use super::tools;
+fn valid(t:Vec<i32>)->bool
+{
+    let mut same = 0;
+ 
+    for i in 0..t.len()-1
+    {
+        let d = t[i+1]- t[i];
+        if !(d.abs()>=1 && d.abs()<=3 && (d.signum()==same || same==0))
+        {
+            return false;
+        }
+        same = d.signum();
+    }
+    true
+}
+
+fn ok(s:&String,sec:bool)->bool
+{
+    let tab = s.split(" ").map(|a| a.parse().unwrap()).collect::<Vec<i32>>();
+
+    if sec
+    {
+        for i in 0..tab.len()
+        {
+            let mut t = tab.clone();
+            t.remove(i);
+
+            if valid(t)            
+            {
+                return true;
+            }
+        }
+
+        false
+    }    
+      else 
+    {
+        valid(tab)
+    }
+}
 
 pub fn part1(data:&[String])->usize
 {
-    let mut v1 = vec![];
-    let mut v2 = vec![];
-
-    data.iter()
-        .for_each(|s| 
-            {
-                let n = tools::split_to_usize(s,"   ");
-                v1.push(n[0]);
-                v2.push(n[1]);
-            }
-        );
-        
-    v1.sort();
-    v2.sort();
-
-    v1.iter()
-      .enumerate()
-      .map(|(i,_)| v1[i].abs_diff(v2[i]))
-      .sum::<usize>()
+   data.iter()
+       .filter(|n| ok(n,false))
+       .count()
 }
 
 pub fn part2(data:&[String])->usize
 {
-    let mut v = vec![];
-    let mut hash = HashMap::new();
-
-    data.iter()
-        .for_each(|s| 
-            {
-                let n= tools::split_to_usize(s,"   ");
-                v.push(n[0]);
-                hash.entry(n[1]).and_modify(|e| *e+=1).or_insert(1);
-            }
-        );
-
-    v.iter()
-     .map(|n| (n*hash.get(n).unwrap_or(&0)))
-     .sum()
+   data.iter()
+       .filter(|n| ok(n,true))
+       .count()
 }
 
 #[allow(unused)]
@@ -55,16 +65,27 @@ pub fn solve(data:&[String])
 fn test1()
 {
     let v = vec![
-        "3   4".to_string(),
+        "7 6 4 2 1".to_string(),
+        "1 2 7 8 9".to_string(),
+        "9 7 6 2 1".to_string(),
+        "1 3 2 4 5".to_string(),
+        "8 6 4 4 1".to_string(),
+        "1 3 6 7 9".to_string(),        
     ];
-    assert_eq!(part1(&v),11);
+    assert_eq!(part1(&v),2);
 }
+
 
 #[test]
 fn test2()
 {
     let v = vec![
-        "3   4".to_string(),
+        "7 6 4 2 1".to_string(),
+        "1 2 7 8 9".to_string(),
+        "9 7 6 2 1".to_string(),
+        "1 3 2 4 5".to_string(),
+        "8 6 4 4 1".to_string(),
+        "1 3 6 7 9".to_string(),        
     ];
-    assert_eq!(part2(&v),31);
+    assert_eq!(part2(&v),4);
 }
