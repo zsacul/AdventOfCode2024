482aad7042f0e26c98ef9963cc14c8a33c05dee3
Łukasz Piwowar <zsacul@gmail.com>
2024-12-11 07:32:56 +0100

    day11

--- src/day11.rs
+++ src/day11.rs
@@ -1,70 +1,55 @@
 use std::collections::HashMap;
 
-fn calc(s:String,i:usize,n:usize,hash:&mut HashMap<(String,usize),usize>)->usize
+fn calc(s:usize,i:usize,n:usize,hash:&mut HashMap<(usize,usize),usize>)->usize
 {
-    if hash.contains_key(&(s.clone(),i))
-    {
-        return hash[&(s,i)];
-    }
-    
-    let st :Vec<usize> = s.split_whitespace().map(|a|a.parse().unwrap()).collect();
-    let mut st2 = vec![];
+    if hash.contains_key(&(s,i)) { return hash[&(s,i)]; }
 
-    for &s in st.iter()
+    let mut stones = vec![];
+
+    if s==0
     {
-        if s==0
-        {
-            st2.push(1);
-        }
-        else if (s.to_string().len()%2)==0
-        {
-            let ss = s.to_string();
-            let s1 = ss[          ..ss.len()/2].parse::<usize>().unwrap();
-            let s2 = ss[ss.len()/2..          ].parse::<usize>().unwrap();
-            st2.push(s1);
-            st2.push(s2);
-        }
-        else {
-            st2.push(s*2024);
-        }
+        stones.push(1);
+    }
+    else if (s.to_string().len()%2)==0
+    {
+        let str = s.to_string();
+        stones.push(str[           ..str.len()/2].parse::<usize>().unwrap());
+        stones.push(str[str.len()/2..           ].parse::<usize>().unwrap());
+    }
+    else 
+    {
+        stones.push(s*2024);
     }
 
     if i==n-1
     {
-        hash.insert((s,i),st2.len());
-        st2.len()
+        hash.insert((s,i),stones.len());
+        stones.len()
     }
-    else
+      else
     {
-        let mut cnt = 0;
-        for &s in st2.iter()
-        {
-            cnt+=calc(s.to_string(),i+1,n,hash)
-        }
+        let cnt = stones.iter().map(|&f| calc(f,i+1,n,hash)).sum();
         hash.insert((s,i),cnt);
         cnt
     }
 
 }
 
-pub fn part1(data:&[String],n:usize)->usize
+pub fn compute(data:&[String],n:usize)->usize
 {
     let mut hash = HashMap::new(); 
-    calc(data[0].to_string(),0,n,&mut hash)
-}
 
-pub fn part2(data:&[String],n:usize)->usize
-{   
-    let mut hash  = HashMap::new(); 
-    calc(data[0].to_string(),0,n,&mut hash)
+    data[0].split_whitespace()
+           .map(|a| calc(a.parse::<usize>().unwrap(),0,n,&mut hash) )
+           .sum()
 }
 
 #[allow(unused)]
 pub fn solve(data:&[String])
 {    
     println!("Day11");
-    println!("part1: {}",part1(data,25));
-    println!("part2: {}",part2(data,75));
+    println!("part1: {}",compute(data,25));
+    println!("part2: {}",compute(data,75));
 }
 
 #[test]
@@ -73,7 +58,7 @@ fn test0()
     let v = vec![
         "125 17".to_string(),
     ];
-    assert_eq!(part2(&v,6),22);
+    assert_eq!(compute(&v,6),22);
 }
 
 #[test]
@@ -82,6 +67,5 @@ fn test1()
     let v = vec![
         "0 1 10 99 999".to_string(),
     ];
-    assert_eq!(part1(&v,1),7);
+    assert_eq!(compute(&v,1),7);
 }
-
