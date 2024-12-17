78739cf479c548361512fe45aa381b663506512d
Łukasz Piwowar <zsacul@gmail.com>
2024-12-17 11:57:16 +0100

    day17 ugly

--- src/day17.rs
+++ src/day17.rs
@@ -1,3 +1,5 @@
+use rand::Rng;
+
 use super::vec2::Vec2;
 use super::tools;
 
@@ -23,6 +25,8 @@ struct Data {
       prog : Vec<Code>,
       i : usize,
       res : Vec<u8>,
+      golden : Vec<u8>,
+      part2 :bool,
 }
 
 impl Data {
@@ -57,15 +61,18 @@ impl Data {
             };
             program.push(code);
         }
-    //).collect();      
-        
+
+        let golden = num.iter().map(|a| *a as u8).collect::<Vec<u8>>();
+             
         Data {
             reg_A : reg_A as u64,
             reg_B : reg_B as u64,
             reg_C : reg_C as u64,
             prog: program,
             i:0,
-            res: vec![]
+            res: vec![],
+            golden,
+            part2:false,
         }
     }
 
@@ -86,7 +93,16 @@ impl Data {
         }
     }
 
-    fn run(&mut self)
+    fn adv(&mut self,n:u8)->u64
+    {       
+        let m = self.combo(n);
+        let a = self.reg_A; // as f64;
+        let b = 2u64.pow(m as u32); // 1<<m;//if m==0 {1} else {2<<m};
+
+        ((a as f64)/(b as f64)) as u64
+    }
+
+    fn run(&mut self)->bool
     {
         let code = self.prog[self.i];
 
@@ -94,15 +110,19 @@ impl Data {
         {
             Code::Adv(n) => {
                 {
-                    println!("bef Adv {} {}",self.reg_A,n);
-                    if self.reg_A!=0 {
-                        
+                    //println!("bef Adv {} {}",self.reg_A,n);
+                    
+                        /*
                         let m = self.combo(n);
                         let a = self.reg_A; // as f64;
-                        let b = 2u64.pow(m as u32); // 1<<m;//if m==0 {1} else {2<<m};
+                        let b = 2u64.pow(m as u64); // 1<<m;//if m==0 {1} else {2<<m};
                         self.reg_A = ((a as f64)/(b as f64)) as u64;
-                    }
-                    println!("Adv {} {}",self.reg_A,n);
+                        */
+
+                        self.reg_A = self.adv(n);
+                        //self.reg_A = self.reg_A/(1<<self.combo(n));                    
+                    
+                    //println!("Adv {} {}",self.reg_A,n);
                 }
             },
             Code::Bxl(n) => {
@@ -118,7 +138,7 @@ impl Data {
             Code::Jnz(n) => {
                 if self.reg_A != 0
                 {
-                    self.i = n as usize;
+                    self.i = (n/2) as usize;
                     self.i-=1;
                 }
             },
@@ -129,28 +149,33 @@ impl Data {
             },
             Code::Out(n) => {
                 {
-                    println!("{:?} {}",self.combo(n),n);
+                    //println!("{:?} {}",self.combo(n),n);
 
                     let c = (self.combo(n)%8) as u8;
+
+                    if self.part2 && self.golden[self.res.len()]!=c
+                    {
+                        return false;
+                    }
                     self.res.push(c);
                 }
             },
             Code::Bdv(n) => {
-                if n == 0
-                {
+                
+                
                     self.reg_B = self.reg_A/(1<<self.combo(n));                    
-                }
+                
             },
             Code::Cdv(n) => {
-                if n == 0
-                {
+                
+                
                     self.reg_C = self.reg_A/(1<<self.combo(n));
-                }
+                
             },
             _ => panic!("Unknown code"),
         }
         
-
+        true
 
     }
 
@@ -159,10 +184,10 @@ impl Data {
         
         let len = self.prog.len();
 
-        println!("{:?}",self.prog);
-        println!("{:?}",self.reg_A);
-        println!("{:?}",self.reg_B);
-        println!("{:?}",self.reg_C);
+        //println!("{:?}",self.prog);
+        //println!("{:?}",self.reg_A);
+        //println!("{:?}",self.reg_B);
+        //println!("{:?}",self.reg_C);
 
         while self.i < len && self.i>=0
         {
@@ -173,10 +198,24 @@ impl Data {
         self.res.clone()
     }
 
-    fn ok2(&self,a:Vec2,b:Vec2,p:Vec2) -> usize
-    {               
-        0
-       }
+    //> 57000
+
+    fn ok2(&mut self)
+    {
+        self.part2 = true;
+        let len = self.prog.len();
+
+        //println!("{:?}",self.prog);
+        //println!("{:?}",self.reg_A);
+        //println!("{:?}",self.reg_B);
+        //println!("{:?}",self.reg_C);
+
+        while self.i < len && self.i>=0
+        {
+            if !self.run() {return;}
+            self.i+=1;
+        } 
+    }
 
     fn count1(&mut self)->String
     {        
@@ -185,7 +224,8 @@ impl Data {
 
     fn count2(&self)->usize
     {
-        0        
+        0
+        //self.ok2().iter().map(|a| a.to_string()).collect::<Vec<String>>().join(",")
     }
 
 }
@@ -195,16 +235,78 @@ pub fn part1(data:&[String])->String
     Data::new(data).count1()
 }
 
+//105_706_278_758_810 too high
+//105_706_277_661_082
+  
 pub fn part2(data:&[String])->usize
 {
-    Data::new(data).count2()
+    let mut d = Data::new(data);
+    let b = d.reg_B;
+    let c = d.reg_C;
+
+    println!("{:?}",d.golden);
+
+    let mut re = 0u64;//281_474_900_000_000;
+    let mut prev = 0;
+
+    //d.golden = vec![2,4,1,5,7,5,1,6,0,3,4,3,5,5,3,0];
+    //d.golden = vec![0,3,5,4,3,0];
+
+    let mut rng = rand::thread_rng();
+    //3110000000
+    loop 
+    {
+//        let res = re;//(re << 19) + 0b0100010100110011010;
+          let res = (re << 14) + 0b10100110011010;
+//
+//
+//10100110011010
+        //
+//"00100010100110011010"
+//100100010100110011010
+        d.reg_A = res as u64;
+        d.reg_B = b;
+        d.reg_C = c;
+        d.i = 0;
+        d.res = vec![];
+        //d.run();
+
+        //>64K
+        //let tab = 
+        d.ok2();
+
+        //hard test up to:
+        //78_000_000_000
+        
+        //2,4,1,5,7,5,1,6,0,3,4,3,5,5,3,0
+        if d.res.len()==d.golden.len()
+        //-10
+        {
+            let del = res-prev;
+            prev = res;
+            //let re = tab.iter().map(|a| a.to_string()).collect::<Vec<String>>().join(",");
+            println!("{:#064b} {:?} = {:?} del={}",res,res,d.res,del);
+            
+            if d.res == d.golden
+            {
+           //     return res;            
+            }   
+        }
+
+
+        if re%1000_000_000==0
+        {
+            println!("{}",res);
+        }
+        re+=1;
+    }
 }
 
 #[allow(unused)]
 pub fn solve(data:&[String])
 {    
     println!("Day17");
-    println!("part1: {}",part1(data));
+    //println!("part1: {}",part1(data));
     println!("part2: {}",part2(data));
 }
 
@@ -249,7 +351,7 @@ fn test3()
         "".to_string(),
         "Program: 5,0,5,1,5,4".to_string(),
     ];
-    let mut d = Data::new(&v);
+    //let mut d = Data::new(&v);
     assert_eq!(part1(&v),"0,1,2");
 }
 
@@ -306,6 +408,32 @@ fn test6()
     assert_eq!(d.reg_B,44354);   
 }
 
+#[test]
+fn test7()
+{
+    let v = vec![
+        "Register A: 2024".to_string(),
+        "Register B: 0".to_string(),
+        "Register C: 0".to_string(),
+        "".to_string(),
+        "Program: 0,3,5,4,3,0".to_string(),
+    ];
+
+    assert_eq!(part2(&v),117440);
+}
+
+#[test]
+fn test8()
+{
+    let v = vec![
+        "Register A: 117440".to_string(),
+        "Register B: 0".to_string(),
+        "Register C: 0".to_string(),
+        "".to_string(),
+        "Program: 0,3,5,4,3,0".to_string(),
+    ];
+    assert_eq!(part1(&v),"0,3,5,4,3,0");
+}
 //00011101
 //    0111
 //   11010 
