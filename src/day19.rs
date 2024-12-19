d02148f39c8d2f6938305bcc8bcffee07b599c16
Łukasz Piwowar <zsacul@gmail.com>
2024-12-19 07:47:14 +0100

    day19 better

--- src/day19.rs
+++ src/day19.rs
@@ -15,14 +15,9 @@ impl Data {
         let lang = sections[0][0].to_string();
         let hash: HashSet<String> = lang.split(", ").map(|s| s.to_string()).collect();
 
-        let prog : Vec<String> = 
-
-        sections[1].iter()
-            .map(|i|
-            {
-                i.to_string()            
-            }
-        ).collect();
+        let prog : Vec<String> =  sections[1].iter()
+                                             .map(|i| { i.to_string() } )
+                                             .collect();
 
         Data {
             tokens: hash,
@@ -33,7 +28,7 @@ impl Data {
 
     fn possible(&mut self,ask:String,memo:&mut HashMap<String,bool>) -> bool
     {
-        if memo.contains_key(&ask) {return memo[&ask]}
+        if memo.contains_key(&ask) { return memo[&ask] }
 
         if self.tok.contains(&ask.clone())
         {
@@ -43,15 +38,11 @@ impl Data {
 
         for i in 1..ask.len()
         {
-            if self.tok.contains(&ask[..i])
+            if self.tok.contains(&ask[..i]) && self.possible(ask[i..].to_string().clone(),memo) 
             {
-                if self.possible(ask[i..].to_string().clone(),memo) 
-                {
-                    memo.insert(ask.clone(),true);
-                    return true;
-                }
-            }
-            
+                memo.insert(ask.clone(),true);
+                return true;
+            }            
         }
 
         memo.insert(ask.clone(),false);
@@ -77,68 +68,54 @@ impl Data {
         res
     }
 
+    fn limit_tokens(&mut self,ask:String)
+    {
+        let tok : Vec<String> = self.tokens.iter()
+                                           .filter( |t| ask.contains(*t) )
+                                           .map(|f| f.to_string())
+                                           .collect::<Vec<String>>();
+        self.tok.clear();
+        for t in tok.iter()
+        {
+            self.tok.insert(t.to_string());
+        }
+    }
+    
     fn ok1(&mut self,ask:String) -> bool
     {
-        let tok : Vec<String> = self.tokens.iter()
-                                           .filter( |t| ask.contains(*t) )
-                                           .map(|f| f.to_string())
-                                           .collect::<Vec<String>>();
-        self.tok.clear();
-        for t in tok.iter()
-        {
-            self.tok.insert(t.to_string());
-        }
         let mut memo = HashMap::new();
-        if self.possible(ask.clone(),&mut memo) {return true}
-
-        false
+        self.limit_tokens(ask.clone());
+        self.possible(ask,&mut memo)        
     }
 
-    
     fn ok2(&mut self,ask:String) -> usize
     {
-        let tok : Vec<String> = self.tokens.iter()
-                                           .filter( |t| ask.contains(*t) )
-                                           .map(|f| f.to_string())
-                                           .collect::<Vec<String>>();
-
-        self.tok.clear();
-        for t in tok.iter()
-        {
-            self.tok.insert(t.to_string());
-        }
         let mut memo = HashMap::new();
-        self.possible2(ask.clone(),&mut memo)
+        self.limit_tokens(ask.clone());
+        self.possible2(ask,&mut memo)
     }
     
 
     fn count1(&mut self)->usize
     {        
-        let mut count=0;
-        for a in self.asked.clone().iter()
-        {
-            if self.ok1(a.to_string())
-            {
-                count+=1;
-            }
-        }
-        count
+        self.asked
+            .clone()
+            .iter()
+            .filter(|a| self.ok1(a.to_string()))
+            .count()
     }
 
     fn count2(&mut self)->usize
     {        
-        let mut count=0;
-        for a in self.asked.clone().iter()
-        {
-            if self.ok1(a.to_string())
-            {
-                count+=self.ok2(a.to_string());
-            }
-        }
-        count
+        let filtered: Vec<String> = self.asked.clone().iter()
+                                    .filter(|a| self.ok1(a.to_string()))
+                                    .cloned()
+                                    .collect();
+
+        filtered.iter()
+                .map(|b| self.ok2(b.to_string()))
+                .sum()
     }
-
-
 }
 
 pub fn part1(data:&[String])->usize
